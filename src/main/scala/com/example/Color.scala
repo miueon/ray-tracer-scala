57cb60544cd0beb8ca88f14f30448f3345e91f8f
miueon <cruxbell@gmail.com>
2023-08-31 15:03:14 +0800

    using ADT to implement the Hittable

--- src/main/scala/com/example/Color.scala
+++ src/main/scala/com/example/Color.scala
@@ -1,6 +1,7 @@
 package com.example
 
 import cats.effect.IO
+import com.example.vec3.Vec3.vec3Ops
 import com.example.vec3.Vec3
 import com.example.vec3.Vec3Ops
 
@@ -17,20 +18,7 @@ object Color:
 
     def value: Vec3 = self
     def writeColor: IO[Unit] = IO {
-
       println(s"${r} ${g} ${b}")
     }
 
-  given (using ops: Vec3Ops[Vec3]): Vec3Ops[Color] with
-    extension (c: Double) def *:(b: Color): Color = ops.*(b)(c)
-    extension (self: Color)
-      def -(b: Color): Color = ops.-(self)(b)
-      def +(b: Color): Color = ops.+(self)(b)
-      def *(c: Double): Color = ops.*(self)(c)
-      def *(b: Color): Color = ops.*(self)(b)
-      def /(c: Double): Color = ops./(self)(c)
-      def lengthSquared: Double = ops.lengthSquared(self)
-      def length: Double = ops.length(self)
-      def dot(b: Color): Double = ops.dot(self)(b)
-      def cross(b: Color): Color = ops.cross(self)(b)
-      def unitVector: Color = ops.unitVector(self)
+  given colorVecOps: Vec3Ops[Color] = vec3Ops
--- src/main/scala/com/example/Hittable.scala
+++ src/main/scala/com/example/Hittable.scala
@@ -1,6 +1,7 @@
-package com.example
+package com.example.hittable
 
 import com.example.vec3.Vec3
+import com.example.Ray
 
 case class HitRecord(p: Vec3, normal: Vec3, t: Double, frontFace: Boolean)
 object HitRecord:
@@ -12,3 +13,61 @@ object HitRecord:
 trait Hittable[A]:
   extension (self: A)
     def hit(r: Ray, rayTmin: Double, rayTmax: Double): Option[HitRecord]
+
+enum HittableObject:
+  case Sphere(center: Vec3, radius: Double)
+  case HList(hitables: List[HittableObject])
+
+object HittableObject:
+  import HittableObject.{Sphere, HList}
+  given SphereHittable: Hittable[Sphere] with
+
+    extension (self: Sphere)
+      override def hit(
+          r: Ray,
+          rayTmin: Double,
+          rayTmax: Double
+      ): Option[HitRecord] =
+        val oc = r.origin - self.center
+        val a = r.direction.lengthSquared
+        val halfB = oc.dot(r.direction)
+        val c = oc.lengthSquared - self.radius * self.radius
+        val discriminant = halfB * halfB - a * c
+
+        val sqrtd = math.sqrt(discriminant)
+        val root1 = (-halfB - sqrtd) / a
+        val root2 = (-halfB + sqrtd) / a
+
+        def mkHitRecord(root: Double): Option[HitRecord] =
+          val p = r.at(root)
+          val outwardNormal = (p - self.center) / self.radius
+          Some(HitRecord(p, outwardNormal, root))
+        if discriminant < 0 then None
+        else if (root1 > rayTmin && root1 < rayTmax) then mkHitRecord(root1)
+        else if (root2 > rayTmin && root2 < rayTmax) then mkHitRecord(root2)
+        else None
+  given HListHittable: Hittable[HList] with
+    extension (self: HList)
+      override def hit(
+          r: Ray,
+          rayTmin: Double,
+          rayTmax: Double
+      ): Option[HitRecord] =
+        self.hitables.foldLeft(Option.empty[HitRecord]) {
+          (acc, hittableObject) =>
+            acc match
+              case Some(hitRecord) =>
+                hittableObject.hit(r, rayTmin, hitRecord.t)
+              case None =>
+                hittableObject.hit(r, rayTmin, rayTmax)
+        }
+  given ObjectHittable: Hittable[HittableObject] with
+    extension (self: HittableObject)
+      def hit(
+          r: Ray,
+          rayTmin: Double,
+          rayTmax: Double
+      ): Option[HitRecord] =
+        self match
+          case s: Sphere => summon[Hittable[Sphere]].hit(s)(r, rayTmin, rayTmax)
+          case h: HList  => summon[Hittable[HList]].hit(h)(r, rayTmin, rayTmax)
